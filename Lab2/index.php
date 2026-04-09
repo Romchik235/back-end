69ca48ecf190d8b07d2c363b9f49c42fa31f64d3
Roman <116812494+Romchik235@users.noreply.github.com>
2026-04-09 16:06:44 +0300

    Add files via upload

---
+++ Lab2/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Lab2</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container">
+
+<h2>Лабораторна № 2</h2>
+
+<div class="subtitle">
+PHP&MySQL. Введення, редагування та видалення даних з БД.<br>
+Сортування даних.
+</div>
+
+<br>
+
+<h2>Душко Роман ІО-32</h2>
+
+<div class="task">
+<b>Варіант 12.</b><br><br>
+Спроєктувати базу даних про документи, переданих виконавцям:<br>
+виконавець, документ, дата передачі, дата повернення.
+</div>
+
+<br><br>
+
+<a class="btn" href="documents.php">Документи</a>
+<a class="btn" href="executors.php">Виконавці</a>
+
+</div>
+
+</body>
+</html>
